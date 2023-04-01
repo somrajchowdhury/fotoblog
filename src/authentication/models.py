fdc531d73868fe299d16ee9eddad6e3ea77176f7
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-04-01 10:42:28 +0530

    Created custom 'User' model and applied initial migration

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Creator'),
+        (SUBSCRIBER, 'Subscriber'),
+    )
+
+    profile_photo = models.ImageField()
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
