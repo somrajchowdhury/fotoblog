b1e8ce01134f8eebd0a00996c590c9ac084d02f7
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-04-03 12:58:12 +0530

    Created form, view (FBV) and template for user login

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+def login_view(request):
+    form = LoginForm()
+    message = ''
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                message = f'Hello {user.username}! Welcome back.'
+            else:
+                message = 'Login failed!'
+
+    context = {'form': form, 'message': message}
+
+    return render(request,
+                  template_name='authentication/login.html',
+                  context=context)
+
