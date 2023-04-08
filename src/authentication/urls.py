f52ee8d0827920d99b94f0bb6e1b7b61a336a7fe
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-04-08 13:50:48 +0530

    Refactored FBVs to CBVs in 'authentication' app

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login_view, logout_view
+from .views import LoginView, LogoutView
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic import View
 from .forms import LoginForm
 
-def login_view(request):
-    form = LoginForm()
-    message = ''
+class LoginView(View):
+    form_class = LoginForm
+    template_name = 'authentication/login.html'
 
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name,
+                      context={'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        message = ''
+
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
                 return redirect('blog:home')
             message = 'Login failed!'
 
-    context = {'form': form, 'message': message}
+        return render(request, self.template_name,
+                      context={'form': form, 'message': message})
 
-    return render(request,
-                  template_name='authentication/login.html',
-                  context=context)
+class LogoutView(View):
 
-def logout_view(request):
-    logout(request)
-    return redirect('authentication:login')
+    @staticmethod
+    def get(self, request):
+        logout(request)
+        return render('authentication:login')
