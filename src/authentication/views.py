30f5b6ec4a84ffdaecd92b22734e4ab45ea0c4a1
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-04-10 00:04:25 +0530

    Created forms and views (CBV) for sign up feature

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import View
+from django.shortcuts import render
+from .forms import SignUpForm
 
 class LoginView(LoginView):
     template_name = 'authentication/login.html'
@@ -6,3 +9,20 @@ class LoginView(LoginView):
 
 class LogoutView(LogoutView):
     template_name = None
+
+class SignUpView(View):
+    form_class = SignUpForm
+    template_name = 'authentication/signup.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name,
+                      context={'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('authentication:login')
+        return render(request, self.template_name,
+                      context={'form': form})
