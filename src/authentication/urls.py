5ae96c5dac946ebdecd0d7a8ecc19a4105361361
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-04-10 00:07:01 +0530

    Added url for signup view in 'authentication' app

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoginView, LogoutView
+from .views import LoginView, LogoutView, SignUpView
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
