ef2900d257df896155c227878277f4cad646e2e4
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-04-10 13:05:48 +0530

    Add missing import for built in 'redirect' function

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SignUpForm
 
 class LoginView(LoginView):
