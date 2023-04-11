2a5615b80074033ff50908c2eb8bf4f9d58df740
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-04-11 23:21:38 +0530

    Created 'Photo' and 'Blog' models for 'blog' app

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Photo(models.Model):
+    image = models.ImageField()
+    caption = models.CharField(max_length=128,
+                               blank=True)
+    uploader = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                                 on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+class Blog(models.Model):
+    photo = models.ForeignKey(to=Photo,
+                              on_delete=models.SET_NULL,
+                              blank=True,
+                              null=True)
+    title = models.CharField(max_length=128)
+    content = models.CharField(max_length=5000)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    starred = models.BooleanField(default=False)
