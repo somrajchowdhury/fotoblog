4c2d76410e8300a2cc7d0ad93084951d01843c9f
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-04-12 22:00:21 +0530

    Created photo upload template and added url

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, PhotoUploadView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
+    path('upload-photo/', PhotoUploadView.as_view(), name='photo_upload'),
 ]
