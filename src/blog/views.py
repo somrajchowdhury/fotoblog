319705a369dfceb389cb5662686894e13ad525d1
Somraj Chowdhury <chowdhury.somraj08@gmail.com>
2023-04-12 21:55:52 +0530

    Created photo upload form and view (CBV)

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
+from .forms import PhotoForm
 
 class HomeView(LoginRequiredMixin, View):
     template_name = 'blog/home.html'
 
     def get(self, request):
         return render(request, self.template_name)
+
+class PhotoUploadView(View):
+    form_class = PhotoForm
+    template_name = 'blog/photo_upload.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name,
+                      context={'form': form})
+
+    def post(self, request):
+        form = self.form_class(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            photo = form.save(commit=False)
+            photo.uploader = request.user
+            photo.save()
+            return redirect('blog:home')
+        return render(request, self.template_name,
+                      context={'form': form})
